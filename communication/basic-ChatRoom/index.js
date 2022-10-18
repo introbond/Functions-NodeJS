68d4b562f17858efef32f302febaf02556af6d62
introbond <introbond.dev@gmail.com>
2022-10-18 08:09:21 +0700

    initialize basic chatroom

---
+++ communication/basic-ChatRoom/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (message) => {
+        io.emit('chat message', message);
+    });
+});
+  
+server.listen(PORT, () => {
+    console.log(`server is running at port ${PORT}`);
+});
